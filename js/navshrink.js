aa05f68840253d85a8b7342a11cf951821e69aaf
BananaMan8 <zapamti38@gmail.com>
2018-05-19 23:32:17 +0200

    nebitno

--- js/navshrink.js
+++ js/navshrink.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
-  	if($(document).scrollTop() > 50) {
+  	if($(document).scrollTop() > 10) {
+    	$('header').addClass('shrink');
+		$('ul').addClass('shrink');
+    }
+    else {
+    $('header').removeClass('shrink',);
+	 $('ul').removeClass('shrink',);
+    }
+  });
+});
+$(document).ready(function() {
+	$(window).scroll(function() {
+  	if($(document).scrollBot() < 50) {
     	$('header').addClass('shrink');
 		$('ul').addClass('shrink');
     }
